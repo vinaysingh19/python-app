7d58ffb5b75dff280e025f74cf0abd411220d6bc
Vinay Singh <vinay.salerts@gmail.com>
2025-11-01 18:01:09 +0530

    added python-app application with k8s deployments

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+import datetime
+#import os
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
+        #'hostname': os.uname().nodename
+        'hostname': socket.gethostname()
+        })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({
+        "status": "healthy"
+        })
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
+
+
+
+
+
+
+
+#'/api/v1/details'
+#'/api/v1/healthz'
